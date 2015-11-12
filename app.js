27062ddd972a6e7da23247516589bfbc67fdc619
thegentleman007 <bennyp95@gmail.com>
2015-11-12 15:26:13 -0700

    added some of the code necessary to implement the login properly, but it's still unfinished. Also has some modification to the search page that was done during the recitation (post request stuff). Search.handlebars

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-var express 	= require('express')
-	, exphbs		= require('express-handlebars')
-  , port      = 3000
-	, path = require('path') //needed for static path
-	, router 	= express.Router();
+var express 	  = require('express')
+var exphbs		  = require('express-handlebars')
+var bodyParser  = require('body-parser')
+var querystring = require('querystring')
+var request = require('request')
+var session     = require('express-session')
+var path        = require('path') //needed for static path
+var router 	    = express.Router();
+var port        = 3000
 
-var app = express();
+var app = express()
 
 app.engine('handlebars', exphbs({defaultLayout: 'auth_base'}));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.use(express.static(path.join(__dirname, 'public'))); //so it can find static files (like the css files)
 
+app.use(session({   //WHAT ARE YOU???
+  cookieName: 'session',
+  secret: 'alsdkfjaclskdjf',
+  resave: false,
+  saveUninitialized: true
+}))
+/*
+app.get('/authorize', function(req, res){
+  var qs = {
+    client_id: cfg.client_id,
+    redirect_uri: cfg.redirect_uri,
+    response_type: 'code'
+  }
+  //client_id=2261696381.1677ed0.1992891961cf4e42830657329b06665c&http://localhost:3000/auth/finalize
+  var query = querystring.stringify(qs)
+
+  var url = 'https://api.instagram.com/oauth/authorize/?' + query
+
+  res.redirect(url)
+})
+*/
 app.get('/', function(req, res) {
   res.render('index', {
 		title: 'Login',
-		layout: base
+		layout: 'base',
+    css: '\\CSS\\ben.css'
   })
 })
 
@@ -40,6 +68,19 @@ app.get('/search', function(req, res){
 		css: "\\CSS\\search.css",
 		title: 'Search'
 	})
+})
+
+app.post('/search', function(req, res){
+  var tagName  = req.body.query
+
+  var options = {
+    url: 'https://api.instagram.com/v1/tags' + tagName + '/media/recent?access_token' + req.session.access_token
+  }
+
+  request.get(options, function(error, response, body){
+    console.log(body)
+  })
+  res.send('good post')
 })
 
 app.listen(port)
