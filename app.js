fa021aa2a50fc9bd85e157dc3da1ecc6b125a294
austen1oo195 <austen1oo195@gmail.com>
2015-11-14 17:49:00 -0700

    Created home/user Routes.js files, moved routes functions from app.js to appropriate Routes files. deleted HTML folder & node-instagram folder.

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express 	  = require('express')
 var exphbs		  = require('express-handlebars')
 var bodyParser  = require('body-parser')
-var querystring = require('querystring')
-var request     = require('request')
 var session     = require('express-session')
 var path        = require('path') //needed for static path
-var cfg         = require('./config')
-var router 	    = express.Router();
+var homeRoutes  = require('./Routes/homeRoutes.js')
+var userRoutes  = require('./Routes/userRoutes.js')
 var port        = 3000;
 
 var app = express();
@@ -15,131 +13,15 @@ app.engine('handlebars', exphbs({defaultLayout: 'auth_base'}));
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use(express.static(path.join(__dirname, 'public'))); //so it can find static files (like the css files)
-
 app.use(session({
   cookieName: 'session',
   secret: 'alsdkfjaclskdjf',
   resave: false,
   saveUninitialized: true
-
-}))
-
-app.get('/sign_out', function(req, res){
-  req.session.access_token = "";
-  console.log('successful sign out')
-  res.redirect('/')
-  //output sign out successful when that gets resolved
-})
-
-app.get('/authorize', function(req, res){
-  var qs = {
-    client_id: cfg.client_id,
-    redirect_uri: cfg.redirect_uri,
-    response_type: 'code'
-  }
-  //client_id=2261696381.1677ed0.1992891961cf4e42830657329b06665c&http://localhost:3000/auth/finalize
-  var query = querystring.stringify(qs)
-  var url = 'https://api.instagram.com/oauth/authorize/?' + query
-
-  res.redirect(url)
-})
-
-app.get('/auth/finalize', function(req, res) {
-  var post_data = {
-    client_id: cfg.client_id,
-    client_secret: cfg.client_secret,
-    redirect_uri: cfg.redirect_uri,
-    grant_type: 'authorization_code',
-    code: req.query.code
-  }
-
-  var options = {
-    url: 'https://api.instagram.com/oauth/access_token',
-    form: post_data
-  }
-
-  request.post(options, function(error, response, body) {
-    var data = JSON.parse(body)
-    req.session.access_token = data.access_token
-    res.redirect('/dashboard')
-  })
-})
-
-app.get('/', function(req, res) {
-  res.render('index', {
-		title: 'Login',
-		layout: 'base',
-    css: '\\CSS\\home.css'
-  });
-});
-
-app.get('/dashboard', function(req, res) {
-  var options = {
-    url: 'https://api.instagram.com/v1/users/self/feed?access_token=' +
-      req.session.access_token
-  };
-  request.get(options, function(error, response, body){
-    var feed = JSON.parse(body);
-
-    res.render('dashboard', {
-      active_dashboard: "active",
-  		css: "\\CSS\\image.css",
-      title: 'Dashboard',
-      feed: feed.data
-    });
-
-  });
-});
-
-
-app.get('/profile', function(req, res) {
-  var options = {
-    url: 'https://api.instagram.com/v1/users/self/?access_token='
-      + req.session.access_token
-  }
-
-  request.get(options, function(error, response, body){
-    var feed = JSON.parse(body);
-    
-  	res.render('profile', {
-      active_profile: "active",
-  		css: "\\CSS\\profile.css",
-      title: 'Profile',
-      feed: feed.data
-  	});
-  });
-});
-
-
-app.get('/search', function(req, res){
-	res.render('search', {
-    active_search: "active",
-    css: "\\CSS\\search.css",
-		title: 'Search',
-	});
-});
-
-app.post('/search', function(req, res){
-  var tagName  = req.body.query;
-  var options = {
-    url: 'https://api.instagram.com/v1/tags/' + tagName +
-      '/media/recent?access_token=' + req.session.access_token
-  };
-
-  request.get(options, function(error, response, body){
-    var feed = JSON.parse(body);
-
-    res.render('search', {
-      active_search: "active",
-      css: "\\CSS\\search.css",
-      title: 'Search',
-      feed: feed.data
-    });
-
-  });
-});
+}));
+app.use('/', homeRoutes);
+app.use('/user', userRoutes);
 
 app.listen(port);
 
